df19a2dd4c732f29c782631401f1b6756f870a56
geekfabio <geekfabio@gmail.com>
2022-08-21 00:34:41 +0100

    finished Welcome screen

--- lib/earbuds_remote/welcome_screen.dart
+++ lib/earbuds_remote/welcome_screen.dart
@@ -2,6 +2,7 @@
 
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -9,10 +10,12 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const String subTitle = "Full control over your oniexpod";
     return Scaffold(
+      extendBodyBehindAppBar: true,
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.white.withOpacity(0.0),
         title: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -38,6 +41,7 @@ class WelcomeScreen extends StatelessWidget {
       ),
       body: Stack(
         children: [
+          // ? Blured background image
           Positioned(
             right: -20,
             child: Transform.rotate(
@@ -53,16 +57,106 @@ class WelcomeScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 4, sigmaY: 3),
-                  child: DecoratedBox(
-                    decoration:
-                        BoxDecoration(color: Colors.white.withOpacity(0.0)),
-                  ),
-                ),
               ),
             ),
           ),
+          //Blured Background circle
+          Positioned(
+            left: -50,
+            bottom: 50,
+            child: Stack(
+              children: [
+                Container(
+                    width: 180,
+                    height: 180,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [
+                          Colors.orange,
+                          Colors.red,
+                          Colors.orange,
+                          Colors.red,
+                        ],
+                      ),
+                    ),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 35, sigmaY: 35),
+                      child: const SizedBox(),
+                    )),
+              ],
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Colors.transparent,
+              width: 400,
+              height: 300,
+              padding: const EdgeInsets.only(
+                right: 10,
+                left: 2,
+                bottom: 10,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 20),
+                    child: const Text(
+                      subTitle,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 40,
+                          color: Color.fromRGBO(4, 13, 37, 1)),
+                    ),
+                  ),
+                  const Spacer(),
+                  //Button lets go
+
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          backgroundColor: const Color.fromRGBO(3, 23, 55, 1),
+                          side: const BorderSide(
+                              color: Color.fromRGBO(3, 23, 55, 1)),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              side: const BorderSide(color: Colors.red)),
+                        ),
+                        child: Row(
+                          // crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text(
+                              "Lets Go",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(width: 8),
+                            Icon(
+                              CupertinoIcons.arrow_right,
+                              color: Colors.white,
+                              size: 18,
+                            )
+                          ],
+                        )),
+                  ),
+                  // ElevatedButton(
+                  //     style: ButtonStyle(
+                  //         minimumSize: MaterialStateProperty<Size?>(100, 50)),
+                  //     onPressed: () {},
+                  //     child: const Text("HELLO"))
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
