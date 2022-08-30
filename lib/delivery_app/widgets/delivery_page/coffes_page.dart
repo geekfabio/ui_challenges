f1c97415f4d911f6fcdc7c869f6631eab5f8bf3d
geekfabio <geekfabio@gmail.com>
2022-08-30 21:45:51 +0100

    added coffe page and resources

---
+++ lib/delivery_app/widgets/delivery_page/coffes_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ui_challenges/delivery_app/controller/delivery_controller.dart';
+import 'package:ui_challenges/delivery_app/widgets/delivery_page/details_text.dart';
+import 'package:ui_challenges/delivery_app/widgets/delivery_page/food_card.dart';
+
+import '../../models/product_model.dart';
+
+class CoffesPage extends StatelessWidget {
+  CoffesPage({Key? key}) : super(key: key);
+
+  late DeliveryController _controller;
+  PageController pageController =
+      PageController(initialPage: 0, viewportFraction: 0.80);
+  @override
+  Widget build(BuildContext context) {
+    _controller = context.watch<DeliveryController>();
+    return Column(
+      children: <Widget>[
+        // container
+        Expanded(
+          child: PageView.builder(
+              scrollDirection: Axis.horizontal,
+              controller: pageController,
+              physics: const BouncingScrollPhysics(),
+              onPageChanged: (int selectedIndex) {
+                _controller.onPageChanged(selectedIndex);
+              },
+              itemCount: coffeList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: <Widget>[
+                    FoodCard(index: index, isCoffe: true),
+                    DetailsText(index: index)
+                    // bottom text
+                  ],
+                );
+              }),
+        ),
+      ],
+    );
+  }
+}
