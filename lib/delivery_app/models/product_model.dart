0e222cd060d3a24223dfd93e2d0dfca63043e2d7
geekfabio <geekfabio@gmail.com>
2022-08-30 21:46:54 +0100

    added coffes products list

--- lib/delivery_app/models/product_model.dart
+++ lib/delivery_app/models/product_model.dart
@@ -3,10 +3,9 @@ import 'package:ui_challenges/delivery_app/style.dart';
 
 class ProductList {
   Color color;
-  String tag; // Fruits
+  String tag;
   String title;
   String image;
-
 
   String market;
   String distance;
@@ -24,32 +23,62 @@ class ProductList {
       this.kcal = "hhasa"});
 }
 
-List<ProductList> productList = [
+List<ProductList> fruitList = [
   ProductList(
     color: AppColors.blue,
     title: "Salad",
-    image: "assets/images/delivery/salad.png",
-    market: "Lynx Market",
+    image: "assets/images/delivery/pessego.png",
+    market: "Store Market",
     distance: "10min",
-    grams: "100 grams",
-    kcal: "437kcal",
+    grams: "250 grams",
+    kcal: "697kcal",
   ),
   ProductList(
     color: AppColors.orangeCard,
     title: "Atom Plate",
-    image: "assets/images/earbuds/logo.png",
-    market: "Big W Market",
+    image: "assets/images/delivery/salad.png",
+    market: "Nosso Super",
     distance: "15min",
-    grams: "240 grams",
+    grams: "052 grams",
     kcal: "850kcal",
   ),
   ProductList(
     color: AppColors.pink,
     title: "Atom Plate",
-    image: "assets/images/earbuds/logo.png",
+    image: "assets/images/delivery/organic.png",
     market: "Colls Market",
     distance: "25min",
-    grams: "150 grams",
+    grams: "052 grams",
     kcal: "676kcal",
+  ),
+];
+
+List<ProductList> coffeList = [
+  ProductList(
+    color: const Color.fromARGB(255, 0, 105, 53),
+    title: "Coffe Basic",
+    image: "assets/images/delivery/coffe1.png",
+    market: "Supermercado",
+    distance: "10min",
+    grams: "052 grams",
+    kcal: "437kcal",
+  ),
+  ProductList(
+    color: const Color.fromARGB(255, 2, 151, 220),
+    title: "Coffe Premiun",
+    image: "assets/images/delivery/coffe2.png",
+    market: "Nosso Super",
+    distance: "15min",
+    grams: "152 grams",
+    kcal: "345kcal",
+  ),
+  ProductList(
+    color: const Color.fromARGB(255, 164, 64, 203),
+    title: "Coffe Delta",
+    image: "assets/images/delivery/coffe3.png",
+    market: "Candando",
+    distance: "25min",
+    grams: "150 grams",
+    kcal: "476kcal",
   ),
 ];
