9f15aabd07237d5538b428262da1c60f1b56d901
geekfabio <geekfabio@gmail.com>
2022-09-01 02:41:00 +0100

    nem style to pokemon screen

---
+++ lib/pokemon_ui/style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {}
+
+class AppColors {
+  //Primary colors
+  static const primaryBlue = Color.fromRGBO(17, 87, 149, 1);
+  static const topBlue = Color.fromRGBO(31, 207, 255, 1);
+  static const centerBlue = Color.fromRGBO(29, 138, 195, 1);
+  static const primaryRed = Color.fromRGBO(215, 18, 0, 1);
+}
+
+class AppImages {
+  static String pokemon1 = "assets/images/pokemon";
+  static String pokemon2 = "";
+  static String pokemon3 = "";
+}
